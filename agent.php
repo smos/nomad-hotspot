79fd0397f7c834e47679c65d556f29dcf71f0416
Seth Mos <Seth.Mos@dds.nl>
2020-12-19 16:45:04 +0100

    Add initial code commit for the agent and functions. Bare bones change management. - needs lint checkers for config files Add a dummy WPA Supplicant file

--- agent.php
+++ agent.php
@@ -1,3 +1,41 @@
 <?php
 
-include("functions.php")
+include("functions.php");
+
+// Some settings
+$looptimer = 1;
+
+echo "Starting up, entering loop\n";
+$status = array();
+$changes = array();
+
+while (true) {
+	// Let's just start with seeing which interfaces work
+	$iflist = interface_status();
+	foreach ($iflist as $ifname => $iface) {
+		// Skip Loopback		
+		if($ifname == "lo")
+			continue;
+
+		if(!isset($status[$ifname])) {
+			// New interface!
+			echo "Adding interface {$ifname}, status {$iface['operstate']}\n";
+			$status[$ifname] = $iflist[$ifname];
+		} else {
+			// We already have this interface, check if it changed
+			if((if_state($status, $ifname) != if_state($iflist, $ifname))) {
+				echo "{$ifname} moved from '". if_state($status, $ifname) ."' to '". if_state($iflist, $ifname) ."'\n";
+				$changes[$ifname] = true;
+			} else {
+				$changes[$ifname] = false;
+			}
+			
+			// Proceed to reload what is required based on this new information
+			$chglist = compare_cfg_files($cfgdir);
+			process_cfg_changes($chglist);
+
+		}
+	}
+	
+	sleep ($looptimer);
+}
--- functions.php
+++ functions.php
@@ -1,2 +1,124 @@
 <?php
 
+// Config files we know about
+$cfgmap = array(
+			"dnsmasq.conf" => "/etc/dnsmasq.conf",
+			"dhcpcd.conf" => "/etc/dhcpcd.conf",
+			"hostapd.conf" => "/etc/hostapd/hostapd.conf",
+			"wpa_supplicant.conf" => "/etc/wpa_supplicant/wpa_supplicant.conf",
+			"sysctl-routed-ap.conf" => "/etc/sysctl.d/sysctl-routed-ap.conf",
+			);
+	
+$cfgdir = "conf";
+
+// Get all our interface information, index by ifname
+function interface_status() {
+	$cmd = "ip -j address show ";
+	exec($cmd, $out, $ret);
+	if($ret > 0)
+		return false;
+
+	$iflist = array();
+	$ifjson = json_decode($out[0], true);
+	
+	foreach($ifjson as $key => $if){
+		$iflist[$if['ifname']] = $if;
+	}
+
+	return $iflist;
+	
+}
+
+// Return interface status
+function if_state($iflist, $name){
+	// Does this interface even exist?
+	if(!isset($iflist[$name]))
+		return false;
+	
+	return $iflist[$name]['operstate'];
+}
+
+// Let's retrieve our list of configuration files and return an array with mtimes
+function cfg_list($dir) {
+	$files = array_diff(scandir($dir), array('..', '.'));
+	//print_r($files);
+	foreach($files as $file) {
+		$mtimes[$file] = filemtime("{$dir}/{$file}");
+	}
+	return $mtimes;
+	
+}
+
+// Returns list of files that have changed
+function compare_cfg_files ($dir) {
+		$cfglist = cfg_list($dir);
+		$chglist = array();
+		global $cfgmap;
+		// If the local file is newer then the installed file we need to proces on it.
+		foreach ($cfglist as $file => $mtime) {
+				if(file_exists($cfgmap[$file])) {
+					if($mtime > filemtime($cfgmap[$file]))
+						$chglist[$file] = $mtime;
+				} else {
+						// Doesn't exist yet
+						$chglist[$file] = 0;
+				}
+				
+		}
+		return $chglist;
+}
+
+function process_cfg_changes($chglist) {	
+	foreach($chglist as $file => $mtime) {
+		switch($file) {
+			case "sysctl-routed-ap.conf":
+			case "wpa_supplicant.conf":
+				copy_config($file);
+				break;			
+			case "dnsmasq.conf":
+			case "hostapd.conf":
+			case "dhcpcd.conf":
+				copy_config($file);
+				restart_service($file);
+				break;
+			default:
+				echo "What is this mythical config file '{$file}' of which you speak?\n";
+		}		
+	}
+}
+
+function restart_service($file) {
+	echo "Noop\n";
+	switch($file) {
+			case "dnsmasq.conf":
+				$cmd = "sudo service dnsmasq reload";
+				exec($cmd, $out, $ret);
+				if($ret > 0)
+					echo "Failed to restart service for {$file} to {$cfgmap[$file]}\n";
+				break;
+			case "hostapd.conf":
+				$cmd = "sudo service hostapd reload";
+				exec($cmd, $out, $ret);
+				if($ret > 0)
+					echo "Failed to restart service for {$file} to {$cfgmap[$file]}\n";
+				break;
+			case "dhcpcd.conf":
+				$cmd = "sudo service dhcpcd reload";
+				exec($cmd, $out, $ret);
+				if($ret > 0)
+					echo "Failed to restart service for {$file} to {$cfgmap[$file]}\n";
+				break;
+			default:
+				echo "What is this mythical service file '{$file}' of which you speak?\n";
+	}
+}
+
+function copy_config($file) {
+	global $cfgmap;
+	global $cfgdir;
+	
+	$cmd = "sudo cp -a {$cfgdir}/{$file} {$cfgmap[$file]}";
+	exec($cmd, $out, $ret);
+	if($ret > 0)
+		echo "Failed to copy config {$file} to {$cfgmap[$file]}\n";
+}
