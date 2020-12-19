948cf3489423dff81e8e086012ca7b9dd6902b7b
Seth Mos <Seth.Mos@dds.nl>
2020-12-19 18:49:57 +0100

    Basic loading an reloading of configurations files work Leaves config in a state the survives a reboot

--- agent.php
+++ agent.php
@@ -13,10 +13,11 @@ while (true) {
 	// Let's just start with seeing which interfaces work
 	$iflist = interface_status();
 	foreach ($iflist as $ifname => $iface) {
-		// Skip Loopback		
+		// Skip Loopback
 		if($ifname == "lo")
 			continue;
 
+		// $changes[$ifname] = false;
 		if(!isset($status[$ifname])) {
 			// New interface!
 			echo "Adding interface {$ifname}, status {$iface['operstate']}\n";
@@ -29,13 +30,14 @@ while (true) {
 			} else {
 				$changes[$ifname] = false;
 			}
-			
-			// Proceed to reload what is required based on this new information
+
+			// Proceed to reload configuration files required based on this new information
 			$chglist = compare_cfg_files($cfgdir);
 			process_cfg_changes($chglist);
 
+			// If we acted on the changes, save current state 
+			$status[$ifname] = $iflist[$ifname];
 		}
 	}
-	
 	sleep ($looptimer);
 }
--- functions.php
+++ functions.php
@@ -8,7 +8,6 @@ $cfgmap = array(
 			"wpa_supplicant.conf" => "/etc/wpa_supplicant/wpa_supplicant.conf",
 			"sysctl-routed-ap.conf" => "/etc/sysctl.d/sysctl-routed-ap.conf",
 			);
-	
 $cfgdir = "conf";
 
 // Get all our interface information, index by ifname
@@ -20,13 +19,12 @@ function interface_status() {
 
 	$iflist = array();
 	$ifjson = json_decode($out[0], true);
-	
+
 	foreach($ifjson as $key => $if){
 		$iflist[$if['ifname']] = $if;
 	}
 
 	return $iflist;
-	
 }
 
 // Return interface status
@@ -34,7 +32,7 @@ function if_state($iflist, $name){
 	// Does this interface even exist?
 	if(!isset($iflist[$name]))
 		return false;
-	
+
 	return $iflist[$name]['operstate'];
 }
 
@@ -46,7 +44,6 @@ function cfg_list($dir) {
 		$mtimes[$file] = filemtime("{$dir}/{$file}");
 	}
 	return $mtimes;
-	
 }
 
 // Returns list of files that have changed
@@ -56,25 +53,24 @@ function compare_cfg_files ($dir) {
 		global $cfgmap;
 		// If the local file is newer then the installed file we need to proces on it.
 		foreach ($cfglist as $file => $mtime) {
-				if(file_exists($cfgmap[$file])) {
-					if($mtime > filemtime($cfgmap[$file]))
-						$chglist[$file] = $mtime;
-				} else {
-						// Doesn't exist yet
-						$chglist[$file] = 0;
-				}
-				
+			if(file_exists($cfgmap[$file])) {
+				if($mtime > filemtime($cfgmap[$file]))
+					$chglist[$file] = $mtime;
+			} else {
+				// Doesn't exist yet
+				$chglist[$file] = 0;
+			}
 		}
 		return $chglist;
 }
 
-function process_cfg_changes($chglist) {	
+function process_cfg_changes($chglist) {
 	foreach($chglist as $file => $mtime) {
 		switch($file) {
 			case "sysctl-routed-ap.conf":
-			case "wpa_supplicant.conf":
 				copy_config($file);
-				break;			
+				break;
+			case "wpa_supplicant.conf":
 			case "dnsmasq.conf":
 			case "hostapd.conf":
 			case "dhcpcd.conf":
@@ -83,12 +79,11 @@ function process_cfg_changes($chglist) {
 				break;
 			default:
 				echo "What is this mythical config file '{$file}' of which you speak?\n";
-		}		
+		}
 	}
 }
 
 function restart_service($file) {
-	echo "Noop\n";
 	switch($file) {
 			case "dnsmasq.conf":
 				$cmd = "sudo service dnsmasq reload";
@@ -108,15 +103,22 @@ function restart_service($file) {
 				if($ret > 0)
 					echo "Failed to restart service for {$file} to {$cfgmap[$file]}\n";
 				break;
+			case "wpa_supplicant.conf":
+				$cmd = "sudo wpa_cli -i wlan1 reconfigure";
+				exec($cmd, $out, $ret);
+				if($ret > 0)
+					echo "Failed to restart service for {$file} to {$cfgmap[$file]}\n";
+				break;
 			default:
 				echo "What is this mythical service file '{$file}' of which you speak?\n";
+				break;
 	}
 }
 
 function copy_config($file) {
 	global $cfgmap;
 	global $cfgdir;
-	
+
 	$cmd = "sudo cp -a {$cfgdir}/{$file} {$cfgmap[$file]}";
 	exec($cmd, $out, $ret);
 	if($ret > 0)
