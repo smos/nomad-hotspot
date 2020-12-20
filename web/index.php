28b90df3f4a669932b4de09c0d798e0c6f305f32
Seth Mos <Seth.Mos@dds.nl>
2020-12-20 21:26:23 +0100

    Basic web UI shows interface state, ip addresses and if processes are running

--- web/index.php
+++ web/index.php
@@ -4,26 +4,28 @@ include "web.php";
 if (preg_match('/\.(?:css|png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 } else {
+	include "../functions.php";
+	$state = read_shm($shm_id, $shm_size);
 	
 	switch($_SERVER["REQUEST_URI"]) {
 		case "/":
 		case "/status":
 			html_head();
-			echo html_status();
-			echo html_processes();
+			echo html_status($state);
+			echo html_processes($state);
 			echo html_foot();	
 			break;
 		case "/config":
 			echo html_head();
-			echo html_config();
+			echo html_config($state);
 			echo html_foot();	
 			break;
 		case "/json":
-			echo send_json();	
+			echo send_json($state);	
 			break;
 		case "/openvpn":
 			echo html_head();
-			echo html_openvpn();
+			echo html_openvpn($state);
 			echo html_foot();	
 			break;
 	}
--- web/web.php
+++ web/web.php
@@ -6,42 +6,50 @@ function html_head(){
 	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 	header("Content-Type: text/html");
 	echo "<html><head><title>Nomad Hotspot</title></head><body>\n";
+	// Menu Header
 	echo "<table><tr>";
 	echo "<td><a href='/status'>Status</a></td>";
 	echo "<td><a href='/config'>Config</a></td>";
-	echo "<td><a href='/processes'>Processes</a></td>";
-	echo "<td><a href='/openvpn'>OpenVPN</a></td>";
+	// echo "<td><a href='/openvpn'>OpenVPN</a></td>";
 	echo "<td><a href='/json'>JSON</a></td>";
 	echo "<tr></table>\n";
 	
 }
 function html_foot(){
-	echo "</body></html>\n";
-		
+	echo "</body></html>\n";		
 }
 
-function html_config(){
-	echo "";
-		
-}
-
-function html_status(){
-	echo "";
-		
-}
-function html_openvpn(){
+function html_config($state){
 	echo "";
 		
 }
 
-function html_processes(){
+function html_status($state){
+	echo "<table><tr><td>Interface</td><td>State</td><td>Adresses</td></tr>\n";
+	foreach ($state['if'] as $ifname => $iface) {	
+		echo "<tr><td>{$ifname}</td><td>". if_state($state['if'], $ifname)."</td><td>". implode(',', if_address($state['if'], $ifname)) ."</td></tr>\n";
+	}
+	echo "</table>";	
+	echo "\n";		
+		
+}
+function html_openvpn($state){
 	echo "";
 		
 }
-function send_json(){
+
+function html_processes($state){
+	echo "<table><tr><td>Process name</td><td>Number</td></tr>\n";
+	foreach ($state['proc'] as $procname => $number) {
+		echo "<tr><td>{$procname}</td><td>{$number}</td></tr>\n";
+	}
+	echo "</table>";	
+	echo "\n";		
+}
+
+
+function send_json($state){
 	header("Content-Type: application/json");
-    require_once('../functions.php');
-	$state = read_shm($shm_id, $shm_size);
 	echo json_encode($state, JSON_PRETTY_PRINT);
 	
 }
