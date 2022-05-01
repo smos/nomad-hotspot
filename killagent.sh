33a9c73ad17f2315ccdf2c5a6c62badde9e40bef
Seth Mos <seth.mos@ddds.nl>
2022-05-01 18:33:46 +0200

    Fix killagent script for differing user ID's

--- killagent.sh
+++ killagent.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kill `ps auxwf|awk '/\/usr\/bin\/php \/home\/pi\/nomad-hotspot\/agent.php/ {print $2}'|tail -1`
+kill `ps auxwf|awk "/\/usr\/bin\/php \/home\/$LOGNAME\/nomad-hotspot\/agent.php/ {print $2}"|tail -1|awk '{print $2}'`
