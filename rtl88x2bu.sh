b68f0a6ff712fe0bf0dbceb8f641d530ba939f46
Seth Mos <seth.mos@ddds.nl>
2022-05-01 22:35:09 +0200

    Add DKMS driver for the Realtek 88x2bu USB Wireless Nics

---
+++ rtl88x2bu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -d "/lib/modules/5.15.32-v8+/build" ]; then
+	sudo apt install linux-headers linux-build bc build-essential
+fi
+
+sudo git clone "https://github.com/RinCat/RTL88x2BU-Linux-Driver.git" /usr/src/rtl88x2bu-git
+sudo sed -i 's/PACKAGE_VERSION="@PKGVER@"/PACKAGE_VERSION="git"/g' /usr/src/rtl88x2bu-git/dkms.conf
+sudo dkms add -m rtl88x2bu -v git
+sudo dkms autoinstall
+
