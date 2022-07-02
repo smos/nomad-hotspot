f4afb5ff3f0e7c8ec85c5a79538d2c4857e276f1
Seth Mos <seth.mos@ddds.nl>
2022-07-02 22:21:11 +0200

    detect 64 bit pi and build accordingly

--- realtek.sh
+++ realtek.sh
@@ -5,6 +5,8 @@ if [ ! -d "/lib/modules/`uname -r`/build" ]; then
 	sudo apt install linux-headers bc build-essential dkms
 fi
 
+PI64=`uname -m`
+
 echo 1 8812au-20210629
 echo 2 8821au-20210708
 echo 3 8814au
@@ -27,15 +29,17 @@ if [ "$option" = "4" ]; then
 	GITREPO="8821cu-20210118"
 fi
 if [ "$option" = "5" ]; then
-	GITREPO="8812au-20210629"
-fi
-if [ "$option" = "6" ]; then
 	GITREPO="88x2bu-20210702"
 fi
 
 if [ "$GITREPO" != "" ]; then
 	sudo git clone https://github.com/morrownr/"$GITREPO".git /usr/src/"$GITREPO"
 	cd /usr/src/"$GITREPO"
-	sudo ./ARM_RPI.sh
+	
+	if [ "$PI64" = "aarch64" ]; then
+		sudo ./ARM64_RPI.sh
+	else
+		sudo ./ARM_RPI.sh
+	fi
 	sudo ./install-driver.sh
 fi
