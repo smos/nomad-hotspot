9f7598cccbbf4cb52627951150af7a91aa76ebc9
Seth Mos <seth.mos@ddds.nl>
2022-07-27 18:12:24 +0200

    add log snippets for dhcpcd and dnsmasq

--- procmon.sh
+++ procmon.sh
@@ -7,6 +7,8 @@ echo "Select 1 for Webserver"
 echo "Select 2 for Agent"
 echo "Select 3 for OpenVPN Client log"
 echo "Select 4 for hostapd log"
+echo "Select 5 for dnsmasq log"
+echo "Select 6 for dhcpcd log"
 
 read select
 if [ "$select" = "1" ]; then
@@ -18,9 +20,13 @@ elif [ "$select" = "2" ]; then
 	sleep 3
 	screen -r nomad-hotspot
 elif [ "$select" = "3" ]; then
-	sudo grep ovpn /var/log/syslog |tail -n20
+	sudo grep -a ovpn /var/log/syslog |tail -n20
 elif [ "$select" = "4" ]; then
-	sudo grep hostap /var/log/syslog |tail -n20
+	sudo grep -a hostap /var/log/syslog |tail -n20
+elif [ "$select" = "5" ]; then
+	sudo grep -a dnsmasq /var/log/syslog |grep -v dhcp |tail -n20
+elif [ "$select" = "6" ]; then
+	sudo grep -a dhcpcd /var/log/syslog |tail -n20
 else
 	echo Not a valid choice made
 fi
