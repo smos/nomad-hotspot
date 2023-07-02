967247086f405826edf75fef289f6c951c9d6668
Seth Mos <seth.mos@dds.nl>
2023-07-02 08:50:31 +0200

    This version of uninstall should properly restore the required files back so it becomes a client again.

--- uninstall.sh
+++ uninstall.sh
@@ -1,31 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Let's restore the original configuration files"
 
 cd ~/nomad-hotspot/orig
-for FILE in `ls `; do
-	case FILE in
-		dnsmasq.conf)
+FILES="dhcpcd.conf
+dnsmasq.conf
+hostapd.conf
+"
+for FILE in $FILES; do
+	case $FILE in
+		"dnsmasq.conf")
 			echo "Found dnsmasq";
-			sudo cp -a dnsmasq.conf /etc/dnsmasq.conf
+			#sudo cp -a dnsmasq.conf /etc/dnsmasq.conf
 			sudo DEBIAN_FRONTEND=noninteractive apt -y purge dnsmasq
-			break;;
-		dhcpcd.conf)
+			continue;;
+		"dhcpcd.conf")
 			echo "Found dhcpcd";
 			sudo cp -a dhcpcd.conf /etc/dhcpcd.conf
 			# Don't remove, just copy original config back
 			#sudo DEBIAN_FRONTEND=noninteractive apt -y purge dhcpcd
-			break;;
-		hostapd.conf)
+			continue;;
+		"wpa_supplicant.conf")
+			echo "Found wpa_supplicant.conf";
+			sudo cp -a wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+			# Don't remove, just copy original config back
+			#sudo DEBIAN_FRONTEND=noninteractive apt -y purge dhcpcd
+			continue;;
+		"hostapd.conf")
 			echo "Found hostapd";
-			sudo cp -a hostapd.conf /etc/hostapd/hostapd.conf
-			sudo DEBIAN_FRONTEND=noninteractive apt -y purge hostapd
-			break;;
+			#sudo cp -a hostapd.conf /etc/hostapd/hostapd.conf
+			#sudo DEBIAN_FRONTEND=noninteractive apt -y purge hostapd
+			sudo rm /etc/hostapd/hostapd.conf
+			continue;;
 		*)
 			echo "Found file "$FILE" that I have no idea what to do with, sorry."
-			break;;
 	esac
+	echo "Next File"
 done
+
+echo "Disable nomad-hotspot service"
+sudo systemctl stop nomad-hotspot.service
+sudo systemctl disable nomad-hotspot.service
+sudo systemctl mask nomad-hotspot.service
 
 echo "Removing IP forwarding Sysctl, don't forget to reboot"
 sudo rm -f /etc/sysctl/sysctl-routed-ap.conf
